85a3ea30a7d928c11365b34b3769c08595012950
Sherif Khaled <sherif.khaleed@gmail.com>
2019-04-22 21:59:09 +0200

    support odoo12 dependencies and python3 env

--- install-odoo.sh
+++ install-odoo.sh
@@ -16,8 +16,8 @@ USAGE(){
       -m    <model>            Model of odoo system (e.g. 'community') [required]
 
       -p    <admin password>   Password for odoo administrator [recommended]
-      -P   <sql password>     setup password for postgresql [recommended]
-      -r <server port>      spsifec server port
+      -sp   <sql password>     setup password for postgresql [recommended]
+      -port <server port>      spsifec server port
       -h                       Print help
 
   OPTIONS (support SSL)
@@ -43,19 +43,14 @@ HERE
 
 main(){
 
- HOST= get_ip
- PORT=8069
- PASS=''
-
-
  check_x64
  check_mem
- check_ubuntu 16.04
+ check_ubuntu
 
-  while builtin getopts "hv:m:p:P:r:s:e:" opt "${@}"; do
+  while builtin getopts "hv:m:p:sp:port:s:e" opt "${@}"; do
     case $opt in
       h)
-        USAGE
+        usage
         exit 0
         ;;
 
@@ -71,11 +66,11 @@ main(){
         PASS=$OPTARG
         check_admin_pass $PASS
         ;;
-      P)
+      sp)
         SQL_PASS=$OPTARG
         check_sql_pass $SQL_PASS
         ;;
-      r)
+      port)
         PORT=$OPTARG
         check_port $PORT
         ;;
@@ -101,11 +96,7 @@ main(){
     esac
   done
 
-  install_odoo
-
 }
-
-
 
 check_root() {
   if [ $EUID != 0 ]; then err "You must run this script as root."; fi
@@ -119,16 +110,16 @@ check_version(){
   ver=$1
   case $ver in
     9)
-       VERSION=9
+       return $ver
       ;;
     10)
-       VERSION=10
+       return $ver
       ;;
     11)
-       VERSION=11
+       return $ver
       ;;
     12 )
-       VERSION=12
+       return $ver
       ;;
     *)
        err "the script not supported this version $ver"
@@ -155,25 +146,27 @@ check_admin_pass(){
   fi
 }
 check_sql_pass(){
-  if [ -z $1 ];then
+  if [ -z $1];then
     err "please enter the postgresql password"
   fi
 }
 check_port(){
   port=$1
-  if [ $port -gt 8090 ] || [ $port -lt 8060 ];then
+  if [ $port -gt 8090 -o $port -lt 8060 ];then
     err "the port number must be between [8060] and [8090]"
   fi
 }
 check_host(){
-  if [ -z $1 ];then
+  host=$1
+  if [ -z $host ];then
     err "please enter your hostname"
   fi
 
-  #RESOLVE="$(dig +short $HOST @resolver1.opendns.com)"
-  #IP= get_ip
-
-  #if [ "$RESOLVE" != "$IP" ]; then err "DNS lookup for $1 resolved to $DIG_IP but didn't match local $IP."; fi
+  need_pkg dnsutils
+  DIG_IP=$(dig +short $1 | grep '^[.0-9]*$' | tail -n1)
+  if [ -z "$DIG_IP" ]; then err "Unable to resolve $1 to an IP address using DNS lookup."; fi
+  get_IP
+  if [ "$DIG_IP" != "$IP" ]; then err "DNS lookup for $1 resolved to $DIG_IP but didn't match local $IP."; fi
 }
 check_email(){
   email=$1
@@ -196,67 +189,6 @@ check_x64() {
   UNAME=`uname -m`
   if [ "$UNAME" != "x86_64" ]; then err "You must run this command on a 64-bit server."; fi
 }
-get_ip(){
-	myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-    host="${myip}"
-    echo $host
-}
-check_apache(){
-	if dpkg -l | grep -q apache2; then err "You must unisntall apache2 first"; fi
-}
-setup_nginx(){
-	apt-get install nginx -y
-	systemctl start nginx
-	systemctl enable nginx
-
-    ufw start
-	ufw allow 'Nginx HTTP'
-
-}
-configure_nginx(){
-
-	cat > /etc/nginx/sites-available/$HOST << HERE
-
-upstream odoo {
-    server 127.0.0.1:$PORT;
-}
-
-server {
-    listen      80 default;
-    server_name $HOST;
-
-    access_log  /var/log/nginx/odoo.access.log;
-    error_log   /var/log/nginx/odoo.error.log;
-
-    proxy_buffers 16 64k;
-    proxy_buffer_size 128k;
-
-    location / {
-        proxy_pass  http://odoo;
-        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
-        proxy_redirect off;
-
-        proxy_set_header    Host            \$host;
-        proxy_set_header    X-Real-IP       \$remote_addr;
-        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header    X-Forwarded-Proto https;
-    }
-
-    location ~* /web/static/ {
-        proxy_cache_valid 200 60m;
-        proxy_buffering on;
-        expires 864000;
-        proxy_pass http://odoo;
-    }
-}
-	
-HERE
-
-ln -s /etc/nginx/sites-available/$HOST /etc/nginx/sites-enabled/$HOST
-rm -f /etc/nginx/sites-enabled/default
-service nginx restart
-
-}
 upgrade_system(){
   echo -e "\n---- Update Server ----"
   apt update
@@ -265,32 +197,54 @@ upgrade_system(){
 configure_ufw(){
   service ufw start
   ufw allow ssh
-  ufw allow $PORT/tcp
+  ufw allow 8069/tcp
   echo "y" | ufw enable $answer
 }
 install_dependencies(){
+#REF  https://www.getopenerp.com/install-odoo-12-on-ubuntu-18-04/
 
-  declare -a dependencies=("git" "python-pip" "postgresql" "postgresql-server-dev-9.5"
-                           "python-all-dev" "python-dev" "python-setuptools" "libxml2-dev"
-                           "libxslt1-dev" "libsasl2-dev" "libldap2-dev" "pkg-config"
-                           "libtiff5-dev" "libjpeg8-dev" "libjpeg-dev" "zlib1g-dev"
+  declare -a dependencies=("git" "postgresql" "postgresql-server-dev-9.5" "libzip-dev"
+                           "libxml2-dev" "build-essential" "wget" "libxslt-dev" "libldap2-dev"
+                           "libxslt1-dev" "libsasl2-dev" "libldap2-dev" "pkg-config" "libsasl2-dev"
+                           "libtiff5-dev" "libjpeg8-dev" "libjpeg-dev" "zlib1g-dev" "node-less"
                            "libfreetype6-dev" "liblcms2-dev" "liblcms2-utils" "libwebp-dev"
-                           "tcl8.6-dev" "tk8.6-dev" "python-tk" "libyaml-dev" "fontconfig")
+                           "tcl8.6-dev" "tk8.6-dev" "libyaml-dev" "fontconfig")
 
-  for (( i = 0; i < ${#dependencies[@]} ; i++ )); do
-      printf "\n**** Installing: ${dependencies[$i]} *****\n\n"
+ declare -a python_env=("python-pip" "python-all-dev" "python-dev" "python-setuptools" "python-tk")
+
+ declare -a python3_env=("python3-pip" "python3-software-properties" "python3-all-dev" "python3-dev"
+                         "python3-setuptools" "python3-tk" "python3-venv" "python3-wheel")
+
+ for (( i = 0; i < ${#dependencies[@]} ; i++ )); do
+     printf "\n**** Basic dependencies installing now: ${dependencies[$i]} *****\n\n"
+
+     # Run each command in array
+     eval "apt-get install ${dependencies[$i]} -y"
+ done
+
+ if [ $VERSION -eq 10 ];then
+
+   for (( i = 0; i < ${#python_env[@]} ; i++ )); do
+       printf "\n**** python env installing now: ${python_env[$i]} *****\n\n"
+
+       # Run each command in array
+       eval "apt-get install ${python_env[$i]} -y"
+   done
+
+elif [ $VERSION -eq 12 ];then
+  for (( i = 0; i < ${#python3_env[@]} ; i++ )); do
+      printf "\n**** python3 env Installing now : ${python3_env[$i]} *****\n\n"
 
       # Run each command in array
-      eval "apt-get install ${dependencies[$i]} -y"
-
-
+      eval "apt-get install ${python3_env[$i]} -y"
   done
+fi
 }
 create_user(){
   #create postgresql user
-  #su - postgres
-  #PGPASSWORD=Admin159# odoo$VERSION -U postgres
-  #exit
+  su - postgres
+  PGPASSWORD=Admin159# odoo$VERSION -U postgres
+  exit
   #create odoo user
   adduser --system --home=/opt/odoo$VERSION --group odoo$VERSION
 
@@ -300,9 +254,15 @@ configure_logs(){
 }
 install_python_dependencies(){
 
-  #install python dependencies
-  pip install -r /opt/odoo$VERSION/doc/requirements.txt
-  pip install -r /opt/odoo$VERSION/requirements.txt
+  if [ $VERSION -eq 10 ];then
+    #install python dependencies
+    pip install -r /opt/odoo$VERSION/doc/requirements.txt
+    pip install -r /opt/odoo$VERSION/requirements.txt
+  elif [ $VERSION -eq 12 ];then
+    #install python3 dependencies
+    pip3 install -r /opt/odoo$VERSION/doc/requirements.txt
+    pip3 install -r /opt/odoo$VERSION/requirements.txt
+  fi
 
   #Install Less CSS via Node.js and npm
   curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
@@ -323,14 +283,14 @@ create_odoo_config(){
   echo "db_port = False" >> /etc/odoo$VERSION-server.conf
   echo "db_user = odoo$VERSION" >> /etc/odoo$VERSION-server.conf
   echo "db_password = False" >> /etc/odoo$VERSION-server.conf
-  echo "addons_path = /opt/odoo$VERSION/addons" >> /etc/odoo$VERSION-server.conf
+  echo "addons_path = /opt/$ODOO_USER/addons" >> /etc/odoo$VERSION-server.conf
 
-  if [ $PORT -ne 0 ];then
+  if [ ! -z $PORT ];then
     echo "xmlrpc_port = $PORT" >> /etc/odoo$VERSION-server.conf
   fi
 }
 create_odoo_service(){
-  echo "[Unit]" >> /lib/systemd/system/odoo$VERSION-server.service
+  echo "[Unit]" >> /lib/systemd/system/odoo-server.service
   echo "Description=Odoo Open Source ERP and CRM" >> /lib/systemd/system/odoo$VERSION-server.service
   echo "Requires=postgresql.service" >> /lib/systemd/system/odoo$VERSION-server.service
   echo "After=network.target postgresql.service" >> /lib/systemd/system/odoo$VERSION-server.service
@@ -370,18 +330,10 @@ install_odoo(){
   install_dependencies
   create_user
   configure_logs
-  git clone https://www.github.com/odoo/odoo --depth 1 --branch $VERSION.0 --single-branch /opt/odoo$VERSION
+  git clone https://www.github.com/odoo/odoo --depth 1 --branch $VERSION --single-branch /opt/odoo$VERSION
   install_python_dependencies
 
   odoo_server_configuration
-
-  if [ ! -z $HOST ];then
-  	setup_nginx
-  	configure_nginx
-  fi
-
-  test_the_server
-  print_url
 }
 test_the_server(){
   systemctl start odoo$VERSION-server
@@ -389,18 +341,11 @@ test_the_server(){
   systemctl status odoo$VERSION-server
 }
 print_url(){
-
-	if [ -z $HOST ];then
-        EXTERNAL_IP= my_ip
-    fi
-
-    if [ -z "$EMAIL" ];then
-  	    echo "http://$HOST"
-  	elif [ -z $HOST ];then
-  		echo "http://$EXTERNAL_IP"
-    else   
-  	    echo "https://$HOST"
-    fi
+  if [ ! -z $HOSTNAME ];then
+    echo http://$HOSTNAME:8069
+  elif [ ! -z $HOSTNAME -a ! -z $PORT ]; then
+    echo http://$HOSTNAME:$PORT
+  fi
 }
 
 main "$@" || exit 1
