30e76e2a21303473000a5f516b2fefbc7bd4d800
Sherif-Khaled <sherif.khaleed@gmail.com>
2019-04-27 11:58:22 +0200

    resolve ssl config bug

--- install-odoo.sh
+++ install-odoo.sh
@@ -210,6 +210,10 @@ configure_ufw(){
   ufw allow $PORT/tcp
   ufw allow 80/tcp
 
+  if [ ! -z $EMAIL ];then
+  	ufw allow 443/tcp
+  fi
+
   echo "y" | ufw enable $answer
 }
 install_dependencies(){
@@ -384,13 +388,14 @@ systemctl restart nginx
 create_ssl_cert(){
   add-apt-repository ppa:certbot/certbot
   apt-get update
-  apt-get install python-certbot-nginx
+  apt-get install python-certbot-nginx -y
   certbot --nginx -d $HOST -d www.$HOST -m $EMAIL --agree-tos
   openssl dhparam -out /etc/nginx/dhparams.pem 4096
   chmod 600 /etc/nginx/dhparams.pem
   service nginx restart
 }
 configur_ssl_server(){
+  rm /etc/nginx/sites-available/$HOST
   cat > /etc/nginx/sites-available/$HOST << HERE
 # Odoo servers
 upstream odoo {
@@ -402,44 +407,55 @@ server {
     server_name www.$HOST $HOST;
 }
 server {
-    listen 443 ssl http2;
-    server_name $HOST;
-    proxy_read_timeout 720s;
-    proxy_connect_timeout 720s;
-    proxy_send_timeout 720s;
+   listen 443 ssl http2;
+   server_name $HOST;
 
-    # Proxy headers
-    proxy_set_header X-Forwarded-Host \$host;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-    proxy_set_header X-Real-IP \$remote_addr;
+   ssl_certificate /etc/letsencrypt/live/$HOST/fullchain.pem;
+   ssl_certificate_key /etc/letsencrypt/live/$HOST/privkey.pem;
+   ssl_session_cache   shared:SSL:10m;
+   ssl_session_timeout 10m;
+   ssl_session_tickets off;
 
-    # SSL parameters
-    ssl_certificate /etc/letsencrypt/live/$HOST/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/$HOST/privkey.pem;
-    ssl_trusted_certificate /etc/letsencrypt/live/$HOST/chain.pem;
+   ssl_dhparam /etc/nginx/dhparams.pem;
 
-    # log files
-    access_log /var/log/nginx/odoo$VERSION.access.log;
-    error_log /var/log/nginx/odoo$VERSION.error.log;
+   ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+   ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
+   ssl_prefer_server_ciphers on;
 
-    # Handle / requests
-    location / {
-       proxy_redirect off;
+   add_header Strict-Transport-Security max-age=15768000;
+
+   ssl_stapling on;
+   ssl_stapling_verify on;
+   ssl_trusted_certificate /etc/letsencrypt/live/$HOST/chain.pem;
+   resolver 8.8.8.8 8.8.4.4;
+
+   access_log /var/log/nginx/odoo$VERSION.access.log;
+   error_log /var/log/nginx/odoo$VERSION.error.log;
+
+   proxy_read_timeout 720s;
+   proxy_connect_timeout 720s;
+   proxy_send_timeout 720s;
+   proxy_set_header X-Forwarded-Host \$host;
+   proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+   proxy_set_header X-Forwarded-Proto \$scheme;
+   proxy_set_header X-Real-IP \$remote_addr;
+
+   location / {
+     proxy_redirect off;
+     proxy_pass http://odoo;
+   }
+
+
+   location ~* /web/static/ {
+       proxy_cache_valid 200 90m;
+       proxy_buffering    on;
+       expires 864000;
        proxy_pass http://odoo;
-    }
+  }
 
-    # Cache static files
-    location ~* /web/static/ {
-        proxy_cache_valid 200 90m;
-        proxy_buffering on;
-        expires 864000;
-        proxy_pass http://odoo;
-    }
-
-    # Gzip
-    gzip_types text/css text/less text/plain text/xml application/xml application/json application/javascript;
-    gzip on;
+  # gzip
+  gzip_types text/css text/less text/plain text/xml application/xml application/json application/javascript;
+  gzip on;
 }
 HERE
 service nginx restart
@@ -460,7 +476,7 @@ install_odoo(){
     server_configuration
   fi
 
-  if [ "$HOST" != "$EXTERNAL_IP" ] && [ -z "$EMAIL" ];then
+  if [ "$HOST" != "$EXTERNAL_IP" ] && [ ! -z $EMAIL ];then
     create_ssl_cert
     configur_ssl_server
   fi
